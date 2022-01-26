2c1caacbce4d1944e19c437f9cdd57700c63185e
Doomgecko <isabourin@millaschool.org>
2022-01-26 13:54:25 -0800

    Updated the battleship game. I went a bit off the book and tried to get a solution to stop the player from being able to win by guessing the same number 3 times in a row (because I had some extra time). It is very primitive, very chunky. I'm sure I'll be able to shorten it after further reading, but this is what I have for now!

--- battleshipScript.js
+++ battleshipScript.js
@@ -1,6 +1,10 @@
-var location1 = 3;
-var location2 = 4;
-var location3 = 5;
+var randomLoc = Math.floor(Math.random() * 5);
+var location1 = randomLoc;
+var location2 = location1 + 1;
+var location3 = location2 + 1;
+var loc1Status = true;
+var loc2Status = true;
+var loc3Status = true;
 var guess;
 var hits = 0;
 var guesses = 0;
@@ -8,15 +12,27 @@ var isSunk = false;
 
 while (isSunk == false) {
     guess = prompt("Ready, Aim, Fire! (Enter a Number from 0-6):");
-    if (guess < 0 || guess > 6) {
-        alert("Invalid cell number! Please keep your input between 1 and 6!")
+    if ((guess == location1 && loc1Status == false)
+        ||(guess == location2 && loc2Status == false)
+        || (guess == location3 && loc3Status == false)){
+        alert("You already hit there!");
+
+    }else if (guess < 0 || guess > 6) {
+        alert("Invalid cell number! Please keep your input between 1 and 6!");
     }else {
         guesses = guesses + 1;
         if (guess == location1 || guess == location2 || guess == location3){
             hits = hits +1;
-            alert("Your missile hit my battleship!")
+            if (guess == location1){
+                loc1Status = false;
+            } else if (guess == location2){
+                loc2Status = false;
+            } else if (guess == location3){
+                loc3Status = false;
+            }
+            alert("Your missile hit my battleship!");
         } else {
-            alert("Your missile fell into the watter harmlessly.")
+            alert("Your missile fell into the watter harmlessly.");
         }
         if (hits==3){
             isSunk = true;
