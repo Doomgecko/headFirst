fc11be3c7d0ba622b924818ce4b86d9f7122813f
Doomgecko <isabourin@millaschool.org>
2022-03-02 09:26:51 -0800

    Created the parseGuess function to deal with invalid guesses.

--- battleShipV2Script.js
+++ battleShipV2Script.js
@@ -1,3 +1,10 @@
+let controller = {
+    guesses: 0,
+    processGuess: function(guess){
+
+    }
+};
+
 let view = {
 displayMessage : function (msg){
     let messageArea = document.getElementById("messageArea");
@@ -28,20 +35,44 @@ let model = {
             let index = ship.locations.indexOf(guess);
             if (index >= 0) {
                 ship.hits[index] = "hit";
+                view.displayHit(guess);
+                view.displayMessage("HIT!");
                 if (this.isSunk(ship)){
+                    view.displayMessage("You Sank My Battleship!");
                     this.shipsSunk++;
                 }
                 return true;
             }
         }
+        view.displayMiss(guess);
+        view.displayMessage("You Missed!")
         return false;
     },
     isSunk: function(ship){
         for (let i = 0; i < this.shipLength; i++){
-            if (ships.hits[i] !== "hit"){
+            if (ship.hits[i] !== "hit"){
                 return false;
             }
         }
         return true;
+    }
+}
+
+
+function parseGuess(guess){
+    let alphabet = ["A", "B", "C,", "D", "E", "F", "G"];
+
+    if (guess === null || guess.length !== 2){
+        alert("Oops, please enter a letter and a number on the board.")
+    } else {
+        let firstChar = guess.charAt (0);
+        let row = alphabet.indexOf(firstChar);
+        let column = guess.charAt (1);
+
+        if (isNaN(row) || isNaN(column)){
+            alert("Oops, that isn't on the board!");
+        }else if (row < 0 || row >= model.boardSize || column < 0 || column >= model.boardSize){
+            alert("Oops, that's off the board!");
+        }
     }
 }
