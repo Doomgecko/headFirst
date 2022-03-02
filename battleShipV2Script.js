0dbcf20f088e0416aad74452fed5270e93a4da5d
Doomgecko <isabourin@millaschool.org>
2022-03-02 09:11:35 -0800

    Created the model object and added functions fire and isSunk to it.

--- battleShipV2Script.js
+++ battleShipV2Script.js
@@ -1,13 +1,47 @@
 let view = {
 displayMessage : function (msg){
-    var messageArea = document.getElementById("messageArea");
+    let messageArea = document.getElementById("messageArea");
     messageArea.innerHTML = msg;
 },
 displayHit : function(location){
-
+    let cell = document.getElementById(location);
+    cell.setAttribute("class","hit");
 },
 displayMiss : function(location){
-
+    let cell = document.getElementById(location);
+    cell.setAttribute("class","miss");
 }
 
 };
+
+let model = {
+    boardSize: 7,
+    numShips: 3,
+    shipLength:3,
+    shipsSunk: 0,
+    ships: [{locations: ["10", "20", "30"], hits: ["", "", ""]},
+            {locations: ["32", "33", "34"], hits: ["", "", ""]},
+            {locations: ["63", "64", "65"], hits: ["", "", ""]}],
+    fire: function(guess){
+        for (let i = 0; i< this.numShips; i++){
+            let ship = this.ships[i];
+            let index = ship.locations.indexOf(guess);
+            if (index >= 0) {
+                ship.hits[index] = "hit";
+                if (this.isSunk(ship)){
+                    this.shipsSunk++;
+                }
+                return true;
+            }
+        }
+        return false;
+    },
+    isSunk: function(ship){
+        for (let i = 0; i < this.shipLength; i++){
+            if (ships.hits[i] !== "hit"){
+                return false;
+            }
+        }
+        return true;
+    }
+}
