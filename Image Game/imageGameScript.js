fdf2354d927e2e714653869cf2d5a58c4802c827
Doomgecko <isabourin@millaschool.org>
2022-03-16 08:48:34 -0700

    Fixed the code to work as intended. The error was a minor typo in a function call.

--- Image Game/imageGameScript.js
+++ Image Game/imageGameScript.js
@@ -1,10 +1,15 @@
 window.onload = init;
 function init() {
-    let image = document.getElementById("zero");
-    image.onclick = showAnswer;
+    let images = document.getElementsByTagName("img");
+    for (let i = 0; i < images.length; i++){
+        images[i].onclick = showAnswer;
+    }
 }
 
-function showAnswer() {
-    let image = document.getElementById("zero");
-    image.src = "zero.jpg";
+function showAnswer(eventObj) {
+    let image = eventObj.target;
+
+    let name = image.id;
+    name = name + ".jpg";
+    image.src = name;
 }
