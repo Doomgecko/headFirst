98ee3f26f5040d7371b5ecd3baebed586029eb1c
Doomgecko <isabourin@millaschool.org>
2022-04-20 09:31:43 -0700

    Robot objects and a prototype working together.

--- inheritanceProjectScript.js
+++ inheritanceProjectScript.js
@@ -16,4 +16,23 @@ Robot.prototype.blinkLights = function(){
 }
 
 let robby = new Robot ("Robby", 1956, "Dr.Morbius");
-let rosy = new Robot ("Rosie", 1962, "George Jetson");
+let rosie = new Robot ("Rosie", 1962, "George Jetson");
+
+robby.onOffSwitch = true;
+robby.makeCoffee = function(){
+    alert("Getting Coffee from Starbucks");
+}
+
+rosie.cleanHouse = function(){
+    alert("Cleaning the house");
+}
+
+
+console.log(robby.name + " was made by " + robby.maker + " in " + robby.year +
+ " and is owned by " + robby.owner);
+robby.makeCoffee();
+robby.blinkLights();
+
+console.log(rosie.name + " was made by " + rosie.maker + " in " + rosie.year +
+    " and is owned by " + rosie.owner);
+rosie.cleanHouse();
